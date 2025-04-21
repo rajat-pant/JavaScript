8f92a528d68e6a5f093fa2f09a01b6ad0f50c030
Rajat Pant <132782873+rajat-pant@users.noreply.github.com>
2025-04-21 19:41:55 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3) //code readability should be high
+
+let name="Rajat"
+let age=18
+let isLoggedIn=false
+
+ //number -> 2 to power 53
+ //bigint
+ //string-> ""
+ //boolean-> true/false
+ //null-> standalone value
+ //undefined-> value is not assigned
+ //symbol-> unique
+
+ //object
+
+ console.log(typeof "Rajat") //print the datatype type
+ console.log(typeof undefined) //undefined
+ console.log(typeof null) //object
