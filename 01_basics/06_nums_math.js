51e1f25627755c3e1146009a03004e58a1207b9f
Rajat Pant <132782873+rajat-pant@users.noreply.github.com>
2025-04-23 20:41:17 +0000

    math

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,23 @@
+const score=400
+console.log(score) //400
+
+const balance = new Number(100)
+console.log(balance)//[Number: 100]
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const otherNumber=23.8966
+
+console.log(otherNumber.toPrecision(3))
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+// **************** Maths *******************
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.7))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.2))
