8374e85b7040f5b605cdfc1f06fbfd8f5a4ad069
Rajat Pant <132782873+rajat-pant@users.noreply.github.com>
2025-05-11 13:23:47 +0000

    ifelse

---
+++ 04_control_flow/one.js
@@ -0,0 +1,29 @@
+//if
+const isUserLoggedIn=true
+const temperature=41
+if(temperature == 41){
+    console.log("less than 50")
+}
+else{
+    console.log("temperture is greater than 50")
+}
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+const balance = 1000
+if(balance<500){
+    console.log("less than 500");
+}
+else if(balance<750){
+    console.log("less than 750");
+}
+else{
+    console.log("less than 1200");
+}
+
+const UserLoggedIn =true
+const debitCard=true
+
+if(userLoggedIn && debitCard){
+    console.log("Allow to buy course");
+}
