eb3ca028ccfd28cb9ac89539f8f07530cf184da0
Rajat Pant <132782873+rajat-pant@users.noreply.github.com>
2025-05-13 08:17:28 +0000

    forin

---
+++ 05_iterations/four.js
@@ -0,0 +1,17 @@
+//for in loop used in objects
+const myObject={
+    js:'javascript',
+    cpp:'C++',
+    rb:"ruby",
+    swift:"swift by apple"
+}
+
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming=["js", "rb", "java", "cpp", "py"]
+
+for(const key in programming){
+    console.log(programming[key]);
+}
